38394f5ef87ad8c56a7aedc1fcaeb64e2b121992
Vinay Panwar <vinaypanwar280@gmail.com>
2025-01-11 09:47:33 +0000

    WIP: Deployment issue.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+// next.config.js
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */  
+  output: 'export', // Export as a static site
+  assetPrefix: isProd ? '/portfolio/' : '', // Update the base path for GitHub Pages
 };
 
 export default nextConfig;
